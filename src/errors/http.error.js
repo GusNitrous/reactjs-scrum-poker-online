3e0a74563f991cf698c31483ec1ec9c31ccf686d
GusNitrous <gusnitrous@gmail.com>
2021-09-10 22:48:48 +0300

    Added: base http error

---
+++ src/errors/http.error.js
@@ -0,0 +1,18 @@
+/**
+ * Auth error.
+ */
+export class HttpError extends Error {
+    static fromAxios({response}) {
+        return new HttpError(
+            response.status,
+            response.statusText,
+            response.data,
+        );
+    }
+
+    constructor(status, statusText, data) {
+        super(statusText);
+        this.status = status;
+        this.data = data;
+    }
+}
