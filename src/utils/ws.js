cb901ad2be9f43e6ecf9dac56e9fa9de76e5d952
GusNitrous <gusnitrous@gmail.com>
2021-09-12 19:05:34 +0300

    Refactoring: get web-socket instance

--- src/utils/ws.js
+++ src/utils/ws.js
@@ -1,4 +1,19 @@
 import { io } from 'socket.io-client';
-export function createWebSocket(token) {
-    return io('http://localhost:9000', {auth: {token}});
+import { getAuthData } from './storage';
+
+
+let ws = null;
+
+
+export function getSocket(recreate = false) {
+    const {jwtToken} = getAuthData();
+    if (!jwtToken) {
+        throw new Error('Auth token is missing');
+    }
+
+    if (recreate || !ws) {
+        ws = io('http://localhost:9000', {auth: {token: jwtToken}});
+    }
+
+    return ws;
 }
