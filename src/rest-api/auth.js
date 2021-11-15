7a3b98e9faf1eab5109328d7aaf8190cbbd8571e
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:14:56 +0300

    Added: logout

--- src/rest-api/auth.js
+++ src/rest-api/auth.js
@@ -1,15 +1,29 @@
-import { HttpError } from '../errors/http.error';
-import { http } from '../utils/http';
-import { setAuthData } from '../utils/storage';
+import {HttpError} from '../errors/http.error';
+import {clearAuthData, setAuthData} from '../utils/auth';
+import * as http from "../utils/http";
+
+const {withAuth} = http;
 
 /**
  * Authorize user and create voting room.
  */
 export function register(userName) {
-    return http.post('/auth/register', {userName})
+    return http.post({url: '/auth/register', data: {userName}})
         .then(({data}) => {
             setAuthData(data);
-        }).catch((err) => {
-            return Promise.reject(HttpError.fromAxios(err));
+        }).catch((err) => Promise.reject(HttpError.fromAxios(err)));
+}
+
+/**
+ * Doing logout user.
+ */
+export function logout() {
+    return withAuth(http.post)({url: '/auth/logout'})
+        .catch((err) => {
+            const httpError = HttpError.fromAxios(err);
+            console.log(httpError);
+        })
+        .finally(() => {
+            clearAuthData();
         });
 }
