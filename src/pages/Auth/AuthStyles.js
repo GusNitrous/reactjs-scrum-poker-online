e7753110f722b21c37bc970cbacd3db453714249
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:08:53 +0300

    Added: auth styles

---
+++ src/pages/Auth/AuthStyles.js
@@ -0,0 +1,17 @@
+export const AuthStyles = {
+    card: {
+        marginTop: '25px',
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: 5,
+        backgroundColor: '#6b74b1'
+    },
+    submit: {
+        margin: '2, 0, 2'
+    },
+}
