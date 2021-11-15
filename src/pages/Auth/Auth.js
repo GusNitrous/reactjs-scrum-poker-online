36e3e9e51733eb833a9096a5fc6263f69121de63
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:09:47 +0300

    Added: auth page

---
+++ src/pages/Auth/Auth.js
@@ -0,0 +1,108 @@
+import {Avatar, Button, Container, CssBaseline, TextField, Typography} from '@material-ui/core';
+import SupervisedUserCircleRoundedIcon from '@material-ui/icons/SupervisedUserCircleRounded';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import {withStyles} from '@material-ui/core/styles';
+import React, {Component} from 'react';
+import * as AuthAPI from '../../rest-api/auth';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import {BAD_REQUEST} from '../../constants/http-status';
+import {isLoggedIn} from "../../utils/auth";
+import {Redirect} from "react-router-dom";
+import {AuthStyles} from "./AuthStyles";
+
+/**
+ * Auth component.
+ */
+class Auth extends Component {
+    state = {
+        isLoading: false,
+        userName: '',
+        errors: {},
+    };
+
+    setIsLoading = (isLoading) => {
+        this.setState({isLoading});
+    }
+
+    setUserName = (userName) => {
+        this.setState({userName});
+    }
+
+    setErrors = (errors) => {
+        this.setState({errors});
+    }
+
+    signIn = () => {
+        const {history} = this.props;
+        const {userName} = this.state;
+        this.setIsLoading(true);
+        AuthAPI.register(userName)
+            .then(() => {
+                // Проверяем referer ссылку с которой пришли на этот адрес
+                // Если ссылка есть, то после регистрации
+                // делаем редирект на referer cсылку, иначе делаем редирект на
+                // quickstart
+                history.push('/quickstart');
+            })
+            .catch((err) => {
+                this.setIsLoading(false);
+                if (err.status === BAD_REQUEST) {
+                    this.setErrors({userName: err.data.message});
+                }
+            });
+    };
+
+    render() {
+        console.log('--- auth_props ---', this.props);
+        // Если пользователь авторизован, то сразу редиректим на quickstart
+        if (isLoggedIn()) {
+            return <Redirect to='/quickstart'/>;
+        }
+
+        const {classes} = this.props;
+        const {errors, isLoading, userName} = this.state;
+        return <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Card className={classes.card}>
+                {isLoading && <LinearProgress/>}
+                <CardContent>
+                    <div className={classes.content}>
+                        <Avatar className={classes.avatar}>
+                            <SupervisedUserCircleRoundedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Авторизация
+                        </Typography>
+                        <TextField
+                            error={!!errors.userName}
+                            helperText={errors.userName}
+                            value={userName}
+                            onInput={(e) => this.setUserName(e.target.value)}
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="userName"
+                            label="UserName"
+                            name="userName"
+                            autoFocus
+                        />
+                        <Button
+                            disabled={!userName || isLoading}
+                            type="buttom"
+                            onClick={this.signIn}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Войти
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </Container>
+    }
+}
+
+export default withStyles(AuthStyles)(Auth);
