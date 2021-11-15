65a03047180d1c97271140b4de8b32acd36cbbaa
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:12:40 +0300

    Refactoring: http utils

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,33 @@
 import axios from 'axios';
-export const http = axios.create({
+import {getAuthData} from "./auth";
+
+export const request = axios.create({
     baseURL: 'http://localhost:9000'
 });
+
+export const get = ({url, config}) => {
+    return request.get(url, config);
+}
+
+export const post = ({url, data, config}) => {
+    return request.post(url, data, config);
+}
+
+export const withAuth = (httpMethod) => {
+    return (opt) => {
+        const authHeaders = {headers: getAuthHeader()};
+        const prop = 'config';
+        opt[prop] = !opt[prop] ? authHeaders : {...opt[prop], ...authHeaders};
+        return httpMethod(opt);
+    }
+}
+
+const getAuthHeader = (prefix = 'Bearer') => {
+    const {jwtToken} = getAuthData() ?? {};
+    if (!jwtToken) {
+        throw new Error('Empty auth token');
+    }
+    return {
+        'Authorization': `${prefix} ${jwtToken}`
+    }
+}
