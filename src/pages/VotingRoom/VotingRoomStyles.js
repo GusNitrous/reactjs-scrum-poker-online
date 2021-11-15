cd5c8580da7fb037b42b0cb7066db3274da404b3
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:09:08 +0300

    Added: voting room styles

---
+++ src/pages/VotingRoom/VotingRoomStyles.js
@@ -0,0 +1,19 @@
+export const VotingRoomStyles = {
+    paper: {
+        marginTop: '0px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: '10px',
+    },
+    form: {
+        width: '100%',
+        marginTop: '10px',
+    },
+    submit: {
+        margin: '2, 0, 2',
+    },
+
+};
