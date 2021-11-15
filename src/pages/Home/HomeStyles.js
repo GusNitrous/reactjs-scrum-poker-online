73e67700d681786de3d908292376fc6882a42ee7
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:13:48 +0300

    Refactoring: home page -> css in js

---
+++ src/pages/Home/HomeStyles.js
@@ -0,0 +1,11 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(() => ({
+    root: {
+        flexGrow: 1,
+    },
+    mainGrid: {
+        padding: 15,
+        textAlign: 'center'
+    },
+}));
