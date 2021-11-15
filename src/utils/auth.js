e0a9e1ea4ea1d7661c5c29c8efc963f8f433f8ca
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:09:58 +0300

    Added: auth utils

---
+++ src/utils/auth.js
@@ -0,0 +1,19 @@
+import {clear, getItem, setItem} from './storage';
+
+export function setAuthData(data) {
+    setItem('auth', data);
+}
+
+export function getAuthData() {
+    return getItem('auth');
+}
+
+export function clearAuthData() {
+    clear();
+}
+
+export function isLoggedIn() {
+    return !!getAuthData();
+}
+
+
