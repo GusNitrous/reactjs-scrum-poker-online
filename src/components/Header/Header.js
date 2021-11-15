da1d7b442bbdfda2213e1599aa156d0ec92e381a
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:15:21 +0300

    Refactoring: header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import {useStyles} from "./HeaderStyles";
+import {useHistory} from "react-router-dom";
+import {isLoggedIn} from "../../utils/auth";
 
-const useStyles = makeStyles((theme) => ({
-  toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbarTitle: {
-    flex: 1,
-  },
-  toolbarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
-  },
-  toolbarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
-}));
-
+/**
+ * Header component
+ */
 export default function Header(props) {
-  const classes = useStyles();
-  const { title } = props;
+    const classes = useStyles();
+    const history = useHistory();
+    const isAuth = isLoggedIn();
+    const {title, render: renderButtons} = props;
 
-  return (
-    <React.Fragment>
-      <Toolbar className={classes.toolbar}>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          className={classes.toolbarTitle}
-        >
-          {title}
-        </Typography>
-      </Toolbar>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Toolbar className={classes.toolbar}>
+                <Typography
+                    component="h2"
+                    variant="h5"
+                    color="inherit"
+                    align="center"
+                    noWrap
+                    className={classes.toolbarTitle}
+                >
+                    {title}
+                </Typography>
+                {renderButtons({isAuth, history})}
+            </Toolbar>
+        </React.Fragment>
+    );
 }
 
 Header.propTypes = {
-  sections: PropTypes.array,
-  title: PropTypes.string,
+    sections: PropTypes.array,
+    title: PropTypes.string,
 };
