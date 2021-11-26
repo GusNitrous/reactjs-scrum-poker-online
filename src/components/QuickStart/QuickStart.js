823a040014d625332f9291090b78dd002cd5d9c1
GusNitrous <gusnitrous@gmail.com>
2021-11-26 22:04:49 +0300

    Refactoring: using room model in QuickStart component

--- src/components/QuickStart/QuickStart.js
+++ src/components/QuickStart/QuickStart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import {
     Button,
     Card,
@@ -7,107 +7,104 @@ import {
     CssBaseline,
     Divider,
     Grid,
-    LinearProgress,
     TextField,
     Typography
 } from '@material-ui/core';
-import {useHistory, useParams} from "react-router-dom";
-import {getSocket} from '../../api/ws';
-import {CREATE_ROOM, JOIN_USER, ROOM_CREATED, USER_JOINED, USER_JOINED_TO_ROOM} from '../../api/ws/events';
-import {useStyles} from "./QuickStartStyles";
+import {QuickStartStyles} from "./QuickStartStyles";
+import {withStyles} from "@material-ui/core/styles";
+import {createRoom, joinToRoom} from "../../models/room";
+import {getSocket} from "../../api/ws";
 
 /**
  * QuickStart page component.
  */
-export const QuickStart = () => {
-    const classes = useStyles();
-    const history = useHistory();
-    const {id} = useParams();
-    const [isLoading, setIsLoading] = useState(false);
-    const [roomId, setRoomId] = useState('');
+class QuickStart extends Component {
+    state = {
+        roomId: '',
+    };
 
-    const socket = () => {
-        return getSocket();
+    constructor(props) {
+        super(props);
+        this.ws = getSocket();
     }
 
-    const createRoom = () => {
-        const ws = socket();
-        ws?.on(ROOM_CREATED, (roomId) => {
-            history.push(`/room/${roomId}`);
-            console.log('created room', roomId);
-        }).emit(CREATE_ROOM);
+    createRoom = () => {
+        createRoom(this.ws);
     }
 
-    const joinRoom = (roomId) => {
-        if (id !== roomId) {
-            history.push(`/room/${roomId}`);
-        }
-
-        const ws = socket();
-        ws?.on(USER_JOINED, (roomId) => {
-            console.log('user joined', roomId);
-        }).emit(JOIN_USER, {roomId});
+    joinToRoom = () => {
+        joinToRoom({ws: this.ws, roomId: this.state.roomId});
     }
 
-    return <Container component="main" maxWidth="xs">
-        <CssBaseline/>
-        <Card>
-            {isLoading && <LinearProgress/>}
-            <CardContent>
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                        Готовы начать голосование?
-                    </Typography>
-                    <Button
-                        type="buttom"
-                        onClick={createRoom}
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Создать комнату
-                    </Button>
+    setRoomId = (roomId) => {
+        this.setState({roomId});
+    }
 
-                    <Grid
-                        className={classes.splitter}
-                        container
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center">
-                        <Grid item xs={3}>
-                            <Divider/>
-                        </Grid>
-                        <Grid item xs={6} className={classes.middleText}>
-                            Уже есть комната?
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Divider/>
-                        </Grid>
-                    </Grid>
+    render() {
+        const {roomId} = this.state;
+        const {classes} = this.props;
+        return <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Card>
+                {/*{isLoading && <LinearProgress/>}*/}
+                <CardContent>
+                    <div className={classes.paper}>
+                        <Typography component="h1" variant="h5">
+                            Готовы начать голосование?
+                        </Typography>
+                        <Button
+                            type="buttom"
+                            onClick={this.createRoom}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Создать комнату
+                        </Button>
 
-                    <TextField
-                        value={roomId}
-                        onInput={(e) => setRoomId(e.target.value)}
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="roomId"
-                        label="RoomId"
-                        name="roomId"
-                    />
-                    <Button
-                        disabled={!roomId}
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        onClick={() => joinRoom(roomId)}
-                        className={classes.submit}
-                    >
-                        Присоединиться
-                    </Button>
-                </div>
-            </CardContent>
-        </Card>
-    </Container>
+                        <Grid
+                            className={classes.splitter}
+                            container
+                            direction="row"
+                            alignItems="center"
+                            justifyContent="center">
+                            <Grid item xs={3}>
+                                <Divider/>
+                            </Grid>
+                            <Grid item xs={6} className={classes.middleText}>
+                                Уже есть комната?
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Divider/>
+                            </Grid>
+                        </Grid>
+
+                        <TextField
+                            value={roomId}
+                            onInput={(e) => this.setRoomId(e.target.value)}
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="roomId"
+                            label="RoomId"
+                            name="roomId"
+                        />
+                        <Button
+                            disabled={!roomId}
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            onClick={this.joinToRoom}
+                            className={classes.submit}
+                        >
+                            Присоединиться
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </Container>;
+    }
 }
+
+export default withStyles(QuickStartStyles)(QuickStart);
--- src/components/QuickStart/QuickStartStyles.js
+++ src/components/QuickStart/QuickStartStyles.js
@@ -1,6 +1,6 @@
 import {makeStyles} from "@material-ui/core/styles";
 
-export const useStyles = makeStyles((theme) => ({
+export const QuickStartStyles = makeStyles((theme) => ({
     paper: {
         marginTop: theme.spacing(0),
         display: 'flex',
