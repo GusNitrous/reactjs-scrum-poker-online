adf084ca74dae464d072879b49cacd55d67fe9f4
GusNitrous <gusnitrous@gmail.com>
2022-01-06 15:58:24 +0300

    Removed: report web-vitals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import { IndexPage } from './pages/IndexPage';
 import './index.css';
 import './models/init';
@@ -11,8 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
