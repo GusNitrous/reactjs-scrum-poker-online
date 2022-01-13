aff6011d3de4a7be86a9846cfed3319b2452a51f
GusNitrous <gusnitrous@gmail.com>
2022-01-13 21:34:42 +0300

    WIP: Dashboard styles

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,42 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
-import {Card, CardContent, CardHeader} from "@material-ui/core";
+import {Card, CardContent, CardHeader, IconButton} from "@material-ui/core";
 import {useStore} from "effector-react";
 import {$room} from "../../models/room";
 import {UserList} from "../UserList/UserList";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        marginTop: "3%",
+        marginTop: "20px",
         backgroundColor: theme.palette.background.paper,
     },
+    header: {
+        backgroundColor: '#2d8dc5',
+        color: '#fff',
+    },
+    title: {
+        textTransform: 'uppercase'
+    }
 }));
 
 export const Dashboard = () => {
     const styles = useStyles();
     const room = useStore($room);
     return <Card className={styles.root}>
-        <CardHeader>
-            <h4>Voting Status</h4>
-        </CardHeader>
+        <CardHeader
+            className={styles.header}
+            title={
+                <Typography align="center" className={styles.title} variant="h6" component="h3">
+                    Voting users
+                </Typography>
+            }
+            action={
+                <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                </IconButton>
+            }/>
         <CardContent>
             <UserList users={room.users}/>
         </CardContent>
