077343211e69c505fb7ee56650dfd4a1ef72e5a8
GusNitrous <gusnitrous@gmail.com>
2022-01-14 23:26:52 +0300

    Fixed: using Container in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,30 +7,33 @@ import {history, Routes} from '../../utils/routing';
 import {AuthButton} from "./AuthButton";
 import {useStore} from "effector-react";
 import {$authUser, doLogout} from "../../models/auth";
+import Container from "@material-ui/core/Container";
 
 /**
  * Header component
  */
 export default function Header({title}) {
-    const classes = useStyles();
+    const styles = useStyles();
     const authUser = useStore($authUser);
     const isLoggedIn = !!authUser?.userName;
     return (
         <>
-            <Toolbar className={classes.toolbar}>
-                <Typography
-                    component="h2"
-                    variant="h5"
-                    color="inherit"
-                    noWrap
-                    className={classes.toolbarTitle}
-                >
-                    {title}
-                </Typography>
-                <AuthButton
-                    isAuth={isLoggedIn}
-                    onLogin={() => history.push(Routes.AUTH)}
-                    onLogout={() => doLogout()}/>
+            <Toolbar className={styles.toolbar}>
+                <Container className={styles.headerContainer} maxWidth="md">
+                    <Typography
+                        component="h2"
+                        variant="h5"
+                        color="inherit"
+                        noWrap
+                        className={styles.toolbarTitle}
+                    >
+                        {title}
+                    </Typography>
+                    <AuthButton
+                        isAuth={isLoggedIn}
+                        onLogin={() => history.push(Routes.AUTH)}
+                        onLogout={() => doLogout()}/>
+                </Container>
             </Toolbar>
         </>
     );
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,6 +1,9 @@
 import {makeStyles} from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
+    headerContainer: {
+        display: 'flex',
+    },
     toolbar: {
         borderBottom: `1px solid ${theme.palette.divider}`,
     },
