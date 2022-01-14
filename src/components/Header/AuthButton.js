30d7fb0bc884836eb2c55e29c0a2bae50fe7c62c
GusNitrous <gusnitrous@gmail.com>
2022-01-14 23:26:09 +0300

    Removed: AuthButtonStyles

--- src/components/Header/AuthButton.js
+++ src/components/Header/AuthButton.js
@@ -1,17 +1,13 @@
 import {Button} from "@material-ui/core";
 import React from "react";
-import {useStyles} from "./AuthButtonStyles";
 
 /**
  * AuthButton.
  */
 export const AuthButton = ({isAuth, onLogin, onLogout }) => {
-    const styles = useStyles();
     const title = isAuth ? 'logout' : 'login';
     const onClick = isAuth ? onLogout : onLogin;
-
     return <Button
-        className={styles.authButton}
         size="small"
         onClick={onClick}>
         {title}
