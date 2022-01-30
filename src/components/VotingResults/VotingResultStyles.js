681c696326355310a3d186add98de58b7e25c4ae
GusNitrous <gusnitrous@gmail.com>
2022-01-30 19:48:30 +0300

    Added: voting results layout and styles

---
+++ src/components/VotingResults/VotingResultStyles.js
@@ -0,0 +1,9 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: 25,
+        // width: '100%',
+        // backgroundColor: theme.palette.background.paper,
+    },
+}));
--- src/components/VotingResults/VotingResults.js
+++ src/components/VotingResults/VotingResults.js
@@ -1,12 +1,39 @@
 import React from "react";
+import {Chip, Divider, Paper} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import {useStyles} from "./VotingResultStyles";
 
 export const VotingResults = ({data}) => {
-    return <div>
-        <h3>Voting Results</h3>
-        <ul>
-            <li>Avg: {data.avg}</li>
-            <li>Max: {data.max}</li>
-            <li>Min: {data.min}</li>
-        </ul>
-    </div>
+    const styles = useStyles();
+    return <Paper className={styles.root}>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography
+                    component='h2'
+                    variant="h4"
+                    align="center">
+                    Average
+                </Typography>
+
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                    <Chip color="primary" label={<h3>{data.avg}</h3>}/>
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="subtitle1" align="center">
+                    Max: {data.max}
+                </Typography>
+                <Divider/>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="subtitle1" align="center">
+                    Min: {data.min}
+                </Typography>
+                <Divider/>
+            </Grid>
+        </Grid>
+    </Paper>
 }
