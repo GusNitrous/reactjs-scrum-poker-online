3d54454dfc800c5a8f8340f402ac4bfbac7d57b1
GusNitrous <gusnitrous@gmail.com>
2022-02-03 21:58:16 +0300

    Refactoring: Playground, PokerCard

--- src/components/CardDeck/PokerCard.js
+++ src/components/CardDeck/PokerCard.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import {Button, Paper, Typography} from "@material-ui/core";
-import {useStyles} from "./PokerCardStyles";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        borderRadius: 4,
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        width: 60,
+        height: 90,
+        margin: 10,
+        padding: 0
+    },
+    score: {
+        height: "100%",
+        minWidth: "100%",
+    },
+}));
 
 export const PokerCard = ({score, isSelected, select}) => {
     const styles = useStyles();
--- src/components/Playground/Playground.js
+++ src/components/Playground/Playground.js
@@ -1,64 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {PokerCard} from "../PokerCard/PokerCard";
-import {makeStyles} from "@material-ui/core/styles";
+import React from 'react';
 import {$voting, sendScore} from "../../models/voting";
-import {debounce} from "lodash/function";
 import {useStore} from "effector-react";
 import {VotingResults} from "../VotingResults/VotingResults";
+import {CardDeck} from "../CardDeck/CardDeck";
 
-const SCORES = [
-    '?',
-    '0',
-    '0.5',
-    '1',
-    '2',
-    '3',
-    '5',
-    '8',
-    '13',
-    '20',
-    '40',
-    '100',
-];
-
-const useStyles = makeStyles((theme) => {
-    return {
-        root: {
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-            alignItems: "center",
-            margin: 'auto',
-            maxWidth: 460
-        },
-        heading: {
-            fontSize: theme.typography.pxToRem(15),
-            fontWeight: theme.typography.fontWeightRegular,
-        },
-    }
-});
 
 export const Playground = () => {
-    const styles = useStyles();
     const {results} = useStore($voting);
-    const [selectedScore, setSelectedScore] = useState('');
-    const selectScore = debounce((score) => {
-        setSelectedScore(score);
-        sendScore(score);
-    }, 250);
-    useEffect(() => {
-        if (!results) {
-            setSelectedScore('');
-        }
-    }, [results]);
-    return !results ? <div className={styles.root}>
-        {SCORES.map((score, index) =>
-            <PokerCard
-                key={index}
-                select={selectScore}
-                isSelected={selectedScore === score}
-                score={score}
-            />
-        )}
-    </div> : <VotingResults data={results}/>
+    return !results
+        ? <CardDeck onSelectCard={sendScore}/>
+        : <VotingResults data={results}/>
 }
