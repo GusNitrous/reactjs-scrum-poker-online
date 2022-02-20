f84e4a69ad43c3f624f6a551a472ee45ba30212e
GusNitrous <gusnitrous@gmail.com>
2022-02-20 18:59:29 +0300

    Draft: card styles

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import cx from 'clsx';
 import {makeStyles} from "@material-ui/core/styles";
 import {Card, CardActions, CardContent, CardHeader, Divider} from "@material-ui/core";
 import {useStore} from "effector-react";
@@ -9,10 +10,69 @@ import {$authUser} from "../../models/auth";
 import {VotingControlPanel} from "./VotingControlPanel";
 import {RoomControlPanel} from "./RoomControlPanel";
 
+const customStyles = {
+    root: {
+        height: '100%',
+        transition: '0.3s',
+        position: 'relative',
+        '&:before': {
+            transition: '0.2s',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            content: '""',
+            display: 'block',
+            backgroundColor: '#d9daf1',
+            borderRadius: '1rem',
+            zIndex: 0,
+            bottom: 0,
+        },
+        '&:hover': {
+            '&:before': {
+                bottom: -6,
+            },
+            '& $card': {
+                boxShadow: '-12px 12px 64px 0 #bcc3d6',
+            },
+        },
+    },
+};
+
 const useStyles = makeStyles((theme) => ({
     root: {
+        height: '100%',
+        transition: '0.3s',
+        position: 'relative',
+        '&:before': {
+            transition: '0.2s',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            content: '""',
+            display: 'block',
+            backgroundColor: '#d9daf1',
+            borderRadius: '1rem',
+            zIndex: 0,
+            bottom: 0,
+        },
+        '&:hover': {
+            '&:before': {
+                bottom: -6,
+            },
+            '& $card': {
+                boxShadow: '-12px 12px 64px 0 #bcc3d6',
+            },
+        },
+    },
+    card: {
         width: '100%',
         backgroundColor: theme.palette.background.paper,
+        zIndex: 1,
+        position: 'relative',
+        borderRadius: '1rem',
+        boxShadow: '0 6px 20px 0 #dbdbe8',
+        transition: '0.4s',
+        height: '100%',
     },
     header: {
         backgroundColor: '#347ec9',
@@ -20,7 +80,7 @@ const useStyles = makeStyles((theme) => ({
         padding: 12
     },
     title: {
-        // fontSize: 18,
+        fontSize: 14,
     },
     cardContent: {
         padding: 10
@@ -38,26 +98,28 @@ export const Dashboard = () => {
     const auth = useStore($authUser);
     const isOwner = auth.userId === ownerId;
 
-    return <Card className={styles.root}>
-        <CardHeader
-            className={styles.header}
-            title={
-                <Typography
-                    align="center"
-                    variant="overline"
-                    component="h3"
-                    className={styles.title}>
-                    Voting round started
-                </Typography>
-            }
-        />
-        <CardContent className={styles.cardContent}>
-            <UserList users={voting.users}/>
-        </CardContent>
-        <CardActions className={styles.actions}>
-            {isOwner && <VotingControlPanel/>}
-        </CardActions>
-        <Divider/>
-        <RoomControlPanel/>
-    </Card>
+    return <div className={styles.root}>
+        <Card className={styles.card}>
+            <CardHeader
+                className={styles.header}
+                title={
+                    <Typography
+                        align="center"
+                        variant="overline"
+                        component="h3"
+                        className={styles.title}>
+                        Voting round started
+                    </Typography>
+                }
+            />
+            <CardContent className={styles.cardContent}>
+                <UserList users={voting.users}/>
+            </CardContent>
+            <CardActions className={styles.actions}>
+                {isOwner && <VotingControlPanel/>}
+            </CardActions>
+            <Divider/>
+            <RoomControlPanel/>
+        </Card>
+    </div>
 }
