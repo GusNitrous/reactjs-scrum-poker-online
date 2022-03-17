883b89b34bc8d02bb32affd47cbaad7629248d05
GusNitrous <gusnitrous@gmail.com>
2022-03-17 22:47:42 +0300

    Fixed: unstable_createMuiStrictModeTheme for skipping waring: findDOMNode is deprecated in StrictMode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexPage } from './pages/IndexPage';
+import {IndexPage} from './pages/IndexPage';
+import {MuiThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme} from '@material-ui/core/styles';
 import './index.css';
 import './models/init';
 
+const theme = createMuiTheme();
+
 ReactDOM.render(
-  <React.StrictMode>
-      <IndexPage />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <MuiThemeProvider theme={theme}>
+            <IndexPage/>
+        </MuiThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
