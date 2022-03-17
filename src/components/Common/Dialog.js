de8a53ba0a0e9c21cf3665ffe0a03e5e7af08c47
GusNitrous <gusnitrous@gmail.com>
2022-03-17 22:22:29 +0300

    Added: custom dialog components

---
+++ src/components/Common/Dialog.js
@@ -0,0 +1,49 @@
+import {IconButton, Typography, withStyles} from "@material-ui/core";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import CloseIcon from "@material-ui/icons/Close";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import React from "react";
+
+const styles = (theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+
+export const DialogTitle = withStyles(styles)((props) => {
+    const {children, classes, onClose, ...other} = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant="h6">
+                {children}
+            </Typography>
+            {onClose ? (
+                <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                    <CloseIcon/>
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+});
+
+export const DialogContent = withStyles((theme) => ({
+    root: {
+        padding: theme.spacing(2),
+        maxWidth: 280,
+    },
+}))(MuiDialogContent);
+
+export const DialogActions = withStyles((theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+    },
+}))(MuiDialogActions);
