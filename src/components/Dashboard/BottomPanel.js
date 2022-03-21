c50eaa220595dad248180dfc1b00381b99615f94
GusNitrous <gusnitrous@gmail.com>
2022-03-21 14:43:54 +0300

    Removed: ShareVoting from bottom panel

--- src/components/Dashboard/BottomPanel.js
+++ src/components/Dashboard/BottomPanel.js
@@ -1,16 +1,10 @@
 import React from "react";
 import {BottomNavigation, BottomNavigationAction} from "@material-ui/core";
-import ShareIcon from "@material-ui/icons/Share";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 export const BottomPanel = () => {
     return <BottomNavigation>
-        <BottomNavigationAction
-            label="Share"
-            icon={<ShareIcon/>}
-
-        />
         <BottomNavigationAction
             label="Leave"
             icon={<ExitToAppIcon/>}
