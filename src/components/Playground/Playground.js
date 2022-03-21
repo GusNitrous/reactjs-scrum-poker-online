2186ec85b749fbdf97452879cd63dbbaf2cae998
GusNitrous <gusnitrous@gmail.com>
2022-03-21 14:42:57 +0300

    Added: ShareVoting into Playground

--- src/components/Playground/Playground.js
+++ src/components/Playground/Playground.js
@@ -3,10 +3,25 @@ import {$voting, sendScore} from "../../models/voting";
 import {useStore} from "effector-react";
 import {VotingResults} from "../VotingResults/VotingResults";
 import {CardDeck} from "../CardDeck/CardDeck";
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import {$room} from "../../models/room";
-import {Button, Grid} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import {ShareVoting} from "../ShareVoting/ShareVoting";
+
+
+export const Playground = () => {
+    const styles = useStyles();
+    const {results} = useStore($voting);
+
+    return <Grid container>
+        <Grid className={styles.header} container alignItems="center" justifyContent="space-between">
+            <ShareVoting/>
+        </Grid>
+
+        <Grid container justifyContent="center">
+            {!results ? <CardDeck onSelectCard={sendScore}/> : <VotingResults data={results}/>}
+        </Grid>
+    </Grid>
+}
 
 const useStyles = makeStyles(() => ({
     header: {
@@ -17,20 +32,3 @@ const useStyles = makeStyles(() => ({
         boxShadow: '0px 0px 4px #ccc'
     }
 }));
-
-export const Playground = () => {
-    const styles = useStyles();
-    const {results} = useStore($voting);
-    const {id: roomId} = useStore($room);
-    return <Grid container>
-        <Grid className={styles.header} container alignItems="center" justifyContent="space-between">
-            <Button size="large" type="text" endIcon={<FileCopyIcon/>}>
-                Room: {roomId}
-            </Button>
-        </Grid>
-
-        <Grid container justifyContent="center">
-            {!results ? <CardDeck onSelectCard={sendScore}/> : <VotingResults data={results}/>}
-        </Grid>
-    </Grid>
-}
--- src/components/ShareVoting/ShareVoting.js
+++ src/components/ShareVoting/ShareVoting.js
@@ -1,10 +1,84 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import LinkIcon from '@material-ui/icons/Link';
 import QRCode from "react-qr-code";
 import {AppDialog, DialogContent, DialogTitle} from "../Common/AppDialog";
+import {useStore} from "effector-react";
+import {$room} from "../../models/room";
+import ShareIcon from "@material-ui/icons/Share";
+
+
+export const ShareVoting = () => {
+    const styles = useStyles();
+    const [link, setLink] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+    const {id: roomId} = useStore($room);
+
+    const handleClose = () => setIsOpen(false);
+
+    useEffect(() => {
+        setLink(window.location.href);
+    }, []);
+    
+    // TODO: moving to common utils
+    const copy = (value) => {
+        navigator
+            .clipboard
+            ?.writeText(value)
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    return <>
+        <Button
+            size="large"
+            type="text"
+            endIcon={<ShareIcon/>}
+            onClick={() => setIsOpen(true)}>
+            Room: {roomId}
+        </Button>
+        <AppDialog onClose={handleClose} open={isOpen}>
+            <DialogTitle onClose={handleClose}>
+                Share this room
+            </DialogTitle>
+            <DialogContent dividers>
+                <Grid container>
+                    <Grid container justifyContent="center">
+                        <QRCode value={link}/>
+                    </Grid>
+                    <Grid container spacing={2} className={styles.buttonsBlock}>
+                        <Grid item xs={12}>
+                            <Button
+                                fullWidth
+                                variant="outlined"
+                                onClick={() => {
+                                    copy(link);
+                                }}
+                                endIcon={<LinkIcon/>}>
+                                Copy link
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button
+                                fullWidth
+                                variant="outlined"
+                                onClick={() => {
+                                    copy(roomId);
+                                }}
+                                endIcon={<FileCopyIcon/>}>
+                                Copy RoomId
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </DialogContent>
+        </AppDialog>
+    </>
+}
 
 const useStyles = makeStyles(() => {
     return {
@@ -13,39 +87,3 @@ const useStyles = makeStyles(() => {
         }
     }
 });
-
-export const ShareVoting = ({isOpen, handleClose}) => {
-    const styles = useStyles();
-
-    return <AppDialog onClose={handleClose} open={isOpen}>
-        <DialogTitle onClose={handleClose}>
-            Share this room
-        </DialogTitle>
-        <DialogContent dividers>
-            <Grid container>
-                <Grid container justifyContent="center">
-                    <QRCode value="test"/>
-                </Grid>
-                <Grid container spacing={2} className={styles.buttonsBlock}>
-                    <Grid item xs={12}>
-                        <Button
-                            fullWidth
-                            variant="outlined"
-                            endIcon={<LinkIcon/>}>
-                            Copy link
-                        </Button>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Button
-                            fullWidth
-                            variant="outlined"
-                            endIcon={<FileCopyIcon/>}>
-                            Copy RoomId
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </DialogContent>
-    </AppDialog>
-}
