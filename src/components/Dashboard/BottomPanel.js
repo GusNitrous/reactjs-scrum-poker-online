8c5e6c4232acbfe720998ed5fac5021bf8259864
GusNitrous <gusnitrous@gmail.com>
2022-03-21 14:40:02 +0300

    Refactoring: BottomPanel

--- src/components/Dashboard/BottomPanel.js
+++ src/components/Dashboard/BottomPanel.js
@@ -1,32 +1,23 @@
-import React, {useState} from "react";
+import React from "react";
 import {BottomNavigation, BottomNavigationAction} from "@material-ui/core";
 import ShareIcon from "@material-ui/icons/Share";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import {ShareVoting} from "../ShareVoting/ShareVoting";
 
 export const BottomPanel = () => {
-    const [openShare, setOpenShare] = useState(false);
+    return <BottomNavigation>
+        <BottomNavigationAction
+            label="Share"
+            icon={<ShareIcon/>}
 
-    return <div>
-        <ShareVoting
-            isOpen={openShare}
-            handleClose={() => setOpenShare(false)}
         />
-        <BottomNavigation>
-            <BottomNavigationAction
-                label="Share"
-                icon={<ShareIcon/>}
-                onClick={() => setOpenShare(true)}
-            />
-            <BottomNavigationAction
-                label="Leave"
-                icon={<ExitToAppIcon/>}
-            />
-            <BottomNavigationAction
-                label="Create"
-                icon={<AddCircleOutlineIcon/>}
-            />
-        </BottomNavigation>
-    </div>
+        <BottomNavigationAction
+            label="Leave"
+            icon={<ExitToAppIcon/>}
+        />
+        <BottomNavigationAction
+            label="Create"
+            icon={<AddCircleOutlineIcon/>}
+        />
+    </BottomNavigation>
 }
