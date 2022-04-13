fa648f5d696db76c0721b7e0f827a68fbf625a88
GusNitrous <gusnitrous@gmail.com>
2022-04-13 21:49:12 +0300

    Refactoring: import main styles and theme

--- src/index.js
+++ src/index.js
@@ -1,57 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexPage from './pages/IndexPage';
-import { createGenerateClassName } from "@material-ui/core/styles";
-import {MuiThemeProvider, unstable_createMuiStrictModeTheme as createTheme} from '@material-ui/core/styles';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 import {JssProvider} from "react-jss";
 import {ConfirmProvider} from "material-ui-confirm";
 
-import './index.css';
+// Import global styles and theme
+import {generateClassName, createTheme, getTheme, baseTheme} from './styles/theme.js';
+import './styles/main.css';
+
+// Import Effector models
 import './models/init';
-
-const muiBaseTheme = createTheme();
-
-const generateClassName = createGenerateClassName({
-  dangerouslyUseGlobalCSS: true
-});
-
-const getTheme = (theme) => ({
-  MuiButton: {
-    root: {
-      "&.MuiButton--chubby": {
-        borderRadius: 50
-      },
-      "&.MuiButton--gradient": {
-        boxShadow: '0px 4px 24px rgba(252, 56, 56, 0.4)',
-        transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
-        background: "linear-gradient(to right, #FFC371, #FF5F6D)",
-        "&:hover": {
-           boxShadow: '0px 4px 24px rgba(252, 56, 56, 0.4)',
-        }
-      }
-    },
-    // label: {
-    //   color: muiBaseTheme.palette.common.white,
-    //   textTransform: "none",
-    //   fontSize: 15,
-    //   fontWeight: 700
-    // },
-    contained: {
-      minHeight: 30,
-      boxShadow: muiBaseTheme.shadows[0],
-      "&$focusVisible": {
-        boxShadow: muiBaseTheme.shadows[0]
-      },
-      "&:active": {
-        boxShadow: muiBaseTheme.shadows[0]
-      },
-      "&$disabled": {
-        boxShadow: muiBaseTheme.shadows[0]
-      }
-    }
-  }
-});
-
 
 ReactDOM.render(
     <React.StrictMode>
@@ -61,7 +20,7 @@ ReactDOM.render(
                     typography: {
                         useNextVariants: true
                     },
-                    overrides: getTheme(muiBaseTheme)
+                    overrides: getTheme(baseTheme)
                 })}>
                 <ConfirmProvider 
                     defaultOptions={{
@@ -71,7 +30,6 @@ ReactDOM.render(
                         },
                         confirmationButtonProps: {autoFocus: true}
                     }}>
-                    {/* Use IndexPage component */}
                     <IndexPage/>
                 </ConfirmProvider>
             </MuiThemeProvider>
---
+++ src/styles/theme.js
@@ -0,0 +1,45 @@
+import { createGenerateClassName } from "@material-ui/core/styles";
+import {unstable_createMuiStrictModeTheme as createTheme} from '@material-ui/core/styles';
+
+export const baseTheme = createTheme();
+
+export const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: true
+});
+
+export const getTheme = () => ({
+  MuiButton: {
+    root: {
+      "&.MuiButton--chubby": {
+        borderRadius: 50
+      },
+      "&.MuiButton--gradient": {
+        boxShadow: '0px 4px 24px rgba(252, 56, 56, 0.4)',
+        transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+        background: "linear-gradient(to right, #FFC371, #FF5F6D)",
+        "&:hover": {
+           boxShadow: '0px 4px 24px rgba(252, 56, 56, 0.4)',
+        }
+      }
+    },
+    label: {
+      color: baseTheme.palette.common.white,
+      textTransform: "none",
+      fontSize: 15,
+      fontWeight: 700
+    },
+    contained: {
+      minHeight: 30,
+      boxShadow: baseTheme.shadows[0],
+      "&$focusVisible": {
+        boxShadow: baseTheme.shadows[0]
+      },
+      "&:active": {
+        boxShadow: baseTheme.shadows[0]
+      },
+      "&$disabled": {
+        boxShadow: baseTheme.shadows[0]
+      }
+    }
+  }
+});
