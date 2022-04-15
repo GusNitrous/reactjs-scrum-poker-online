5fdaf47531d9031fe16beb9a302b96683f902e99
GusNitrous <gusnitrous@gmail.com>
2022-04-15 08:49:47 +0300

    Added: Header styles for the Dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,12 +9,53 @@ import {$authUser} from "../../models/auth";
 import {VotingActions} from "./VotingActions";
 import {BottomPanel} from "./BottomPanel";
 import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
+import { useContainedCardHeaderStyles } from '@mui-treasury/styles/cardHeader/contained';
+import { useSoftRiseShadowStyles } from '@mui-treasury/styles/shadow/softRise';
+import { useFadedShadowStyles } from '@mui-treasury/styles/shadow/faded';
 import cx from 'clsx';
 
+
+const useStyles = makeStyles(({palette, breakpoints, spacing}) => ({
+    root: {
+        width: '100%',
+        marginTop: spacing(10),
+        paddingTop: 10,
+        backgroundColor: palette.background.paper,
+        position: 'relative',
+        borderRadius: spacing(1),
+        boxShadow: '0 6px 20px 0 #dbdbe8',
+        overflow: 'initial',
+        [breakpoints.between('lg', 'xl')]: {
+            marginTop: 82,
+        },
+    },
+    header: {
+        background: 'linear-gradient(to right, #5175B4, #FF8383)',
+        borderRadius: spacing(1),
+        width: '80%',
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    content: {
+        padding: 10,
+    },
+    actions: {
+        padding: 25,
+        flexDirection: 'column',
+        flexGrow: 1,
+    },
+}));
 
 export const Dashboard = () => {
     const styles = useStyles();
     const shadowStyles = useOverShadowStyles();
+
+    const cardHeaderStyles = useContainedCardHeaderStyles();
+    const cardShadowStyles = useSoftRiseShadowStyles({ inactive: true });
+    const cardHeaderShadowStyles = useFadedShadowStyles();
 
     const {id: roomId, ownerId, voting} = useStore($room);
     const auth = useStore($authUser);
@@ -22,7 +63,7 @@ export const Dashboard = () => {
 
     return <>
         <Card className={cx(styles.root, shadowStyles.root)}>
-            <CardHeader
+            {/* <CardHeader
                 className={styles.header}
                 title={
                     <Typography
@@ -30,10 +71,19 @@ export const Dashboard = () => {
                         variant="overline"
                         component="h3"
                         className={styles.title}>
-                        Voting round started
+                        Voting started
                     </Typography>
                 }
+            /> */}
+
+
+            <CardHeader
+                className={cx(cardHeaderShadowStyles.root, styles.header)}
+                classes={cardHeaderStyles}
+                title='Voting started'
+                // subheader={'In reversed order'}
             />
+
             <CardContent className={styles.cardContent}>
                 <UserList users={voting.users}/>
             </CardContent>
@@ -45,32 +95,3 @@ export const Dashboard = () => {
         </Card>
     </>
 }
-
-const useStyles = makeStyles(({palette, breakpoints, spacing}) => ({
-    root: {
-        width: '100%',
-        backgroundColor: palette.background.paper,
-        position: 'relative',
-        borderRadius: spacing(1),
-        boxShadow: '0 6px 20px 0 #dbdbe8',
-        [breakpoints.between('lg', 'xl')]: {
-            marginTop: 52,
-        },
-    },
-    header: {
-        backgroundColor: '#5d82c7',
-        color: '#fafafa',
-        padding: 12
-    },
-    title: {
-        fontSize: 14,
-    },
-    cardContent: {
-        padding: 10
-    },
-    actions: {
-        padding: 25,
-        flexDirection: 'column',
-        flexGrow: 1
-    }
-}));
