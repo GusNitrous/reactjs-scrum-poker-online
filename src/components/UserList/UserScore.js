b4306255274d56263289cc6f1416ed1ccae22413
GusNitrous <gusnitrous@gmail.com>
2022-04-17 22:53:08 +0300

    Fixed: UserScore styles

--- src/components/UserList/UserScore.js
+++ src/components/UserList/UserScore.js
@@ -2,7 +2,6 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import ListItemText from "@material-ui/core/ListItemText";
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import {Chip} from "@material-ui/core";
 import {useStore} from "effector-react";
 import {$voting} from "../../models/voting";
 
@@ -11,6 +10,17 @@ const useStyles = makeStyles(() => ({
     root: {
         display: 'flex',
         justifyContent: 'end',
+        color: '#6877ad'
+    },
+    score: {
+        background: '#ec8188',
+        color: '#fff',
+        width: 28,
+        height: 28,
+        borderRadius: '100%',
+        lineHeight: '28px',
+        textAlign: 'center',
+        fontSize: 16
     }
 }));
 
@@ -20,11 +30,11 @@ export const UserScore = ({score}) => {
     const isShowScore = !!results;
 
     const scoreDisplay = score && isShowScore
-        ? <Chip color="primary" label={score}/>
+        ? <div className={styles.score}>{score}</div>
         : (!score
             // TODO using animation
             ? "N/A"
-            : <CheckCircleIcon color="primary"/>);
+            : <CheckCircleIcon color="inherit"/>);
 
     return <ListItemText className={styles.root}>
         {scoreDisplay}
