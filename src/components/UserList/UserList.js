ef401a89423b0d3aa7ac9364d54bfd719aa4f072
GusNitrous <gusnitrous@gmail.com>
2022-04-17 22:59:38 +0300

    Fixed: import UserListItem

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import List from '@material-ui/core/List';
-import {UserItemList} from "./UserItemList";
+import {UserListItem} from "./UserListItem";
 import avatar from '../../assets/images/avatars/man-with-beard.svg';
 
 export const UserList = ({users}) => {
     return <List>
-        {users.map((user, index) => <UserItemList
+        {users.map((user, index) => <UserListItem
                 key={user.id}
                 username={user.name}
                 avatar={avatar}
