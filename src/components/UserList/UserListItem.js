27ff3d7cec01102baee523734f3c4aa53360e442
GusNitrous <gusnitrous@gmail.com>
2022-04-17 22:38:25 +0300

    Fixed: moving Avatar to assets; renaming UserListItem component

---
+++ src/components/UserList/UserListItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import {makeStyles} from "@material-ui/core/styles";
+import {UserBadge} from "./UserBadge";
+import {Divider} from "@material-ui/core";
+import PropTypes from "prop-types";
+import {UserScore} from "./UserScore";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        flex: '1',
+        justifyContent: "space-between"
+    },
+    itemText: {
+        display: 'flex',
+        justifyContent: 'end',
+    }
+}));
+
+export const UserListItem = ({avatar, username, score, hasDivider}) => {
+    const styles = useStyles();
+
+    return <>
+        <ListItem className={styles.root}>
+            <ListItemAvatar>
+                <UserBadge
+                    variant="dot"
+                    overlap="circular"
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                >
+                    <Avatar
+                        alt={username}
+                        src={avatar}/>
+                </UserBadge>
+            </ListItemAvatar>
+            <ListItemText primary={username}/>
+            <UserScore score={score}/>
+        </ListItem>
+        {hasDivider && <Divider/>}
+    </>
+}
+
+UserListItem.propTypes = {
+    avatar: PropTypes.string,
+    username: PropTypes.string,
+    score: PropTypes.string,
+    hasDivider: PropTypes.bool
+}
