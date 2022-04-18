c2fe040a436833fdd6b4db706e6e23d3ee1d45f2
GusNitrous <gusnitrous@gmail.com>
2022-04-18 15:07:34 +0300

    Added: change state for the Dashboard header

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import {Card, CardActions, CardContent, CardHeader, Divider} from "@material-ui/core";
 import {useStore} from "effector-react";
 import {$room} from "../../models/room";
+import {$voting} from "../../models/voting";
 import {UserList} from "../UserList/UserList";
 import {$authUser} from "../../models/auth";
 import {VotingActions} from "./VotingActions";
@@ -13,25 +14,30 @@ import { useFadedShadowStyles } from '@mui-treasury/styles/shadow/faded';
 import cx from 'clsx';
 
 
-const useStyles = makeStyles(({palette, breakpoints, spacing}) => ({
+const useStyles = makeStyles(({palette, spacing}) => ({
     root: {
         width: '100%',
-        marginTop: spacing(10),
+        marginTop: 58,
         paddingTop: 10,
         backgroundColor: palette.background.paper,
         position: 'relative',
         borderRadius: spacing(1),
         boxShadow: '0 6px 20px 0 #dbdbe8',
         overflow: 'initial',
-        [breakpoints.between('lg', 'xl')]: {
-            marginTop: 82,
-        },
     },
     header: {
-        background: 'linear-gradient(to right, #5175B4, #FF8383)',
+        backgroundSize: '200% 200%',
+        backgroundImage: 'linear-gradient(to right, #5175B4, #FF8383)',
+        transition: 'all 0.5s ease-in-out 0s',
         borderRadius: spacing(1),
         width: '80%',
         textAlign: 'center',
+    },
+    finished: {
+        backgroundSize: '100% 100%',
+    },
+    vatingResuls: {
+
     },
     title: {
         fontSize: 15,
@@ -50,20 +56,25 @@ const useStyles = makeStyles(({palette, breakpoints, spacing}) => ({
 export const Dashboard = () => {
     const styles = useStyles();
     const shadowStyles = useOverShadowStyles();
-
     const cardHeaderStyles = useContainedCardHeaderStyles();
     const cardHeaderShadowStyles = useFadedShadowStyles();
 
+    const {results} = useStore($voting);
     const {id: roomId, ownerId, voting} = useStore($room);
     const auth = useStore($authUser);
     const isOwner = auth.userId === ownerId;
+    const isShowScore = !!results;
+
+    const headerStyles = isShowScore 
+        ? cx(cardHeaderShadowStyles.root, styles.header, styles.finished)
+        : cx(cardHeaderShadowStyles.root, styles.header)
 
     return <>
         <Card className={cx(styles.root, shadowStyles.root)}>
             <CardHeader
-                className={cx(cardHeaderShadowStyles.root, styles.header)}
+                className={headerStyles}
                 classes={cardHeaderStyles}
-                title='Voting started'
+                title={isShowScore ? 'Voting finished' : 'Voting started'}
             />
 
             <CardContent className={styles.cardContent}>
@@ -72,7 +83,7 @@ export const Dashboard = () => {
             <CardActions className={styles.actions}>
                 {isOwner && <VotingActions/>}
             </CardActions>
-            <Divider/>
+            <Divider light/>
             <BottomPanel roomId={roomId}/>
         </Card>
     </>
