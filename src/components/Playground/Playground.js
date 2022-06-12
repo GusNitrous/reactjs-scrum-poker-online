c8a272fa798221f5ffbafeaaabe93c81255b01e4
GusNitrous <gusnitrous@gmail.com>
2022-06-12 23:04:27 +0300

    Added: scrolling to top the window after showing results

--- src/components/Playground/Playground.js
+++ src/components/Playground/Playground.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {$voting, sendScore} from "../../models/voting";
 import {useStore} from "effector-react";
 import {VotingResults} from "../VotingResults/VotingResults";
@@ -7,10 +7,24 @@ import {Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {ShareVoting} from "../ShareVoting/ShareVoting";
 
+const useStyles = makeStyles(() => ({
+    shareIcon: {
+        boxShadow: '0px 0px 4px #ccc'
+    }
+}));
 
 export const Playground = () => {
     const styles = useStyles();
     const {results} = useStore($voting);
+
+    useEffect(() => {
+        if (results) {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        }
+    }, [results]);
 
     return <Grid container>
         <Grid className={styles.header} container alignItems="center" justifyContent="space-between">
@@ -18,17 +32,12 @@ export const Playground = () => {
         </Grid>
 
         <Grid container justifyContent="center">
-            {!results ? <CardDeck onSelectCard={sendScore}/> : <VotingResults data={results}/>}
+            {!results
+                ? <CardDeck onSelectCard={sendScore}/>
+                : <VotingResults data={results}/>
+            }
         </Grid>
     </Grid>
 }
 
-const useStyles = makeStyles(() => ({
-    header: {
-        // marginBottom: 10,
-        // paddingRight: 12,
-    },
-    shareIcon: {
-        boxShadow: '0px 0px 4px #ccc'
-    }
-}));
+
