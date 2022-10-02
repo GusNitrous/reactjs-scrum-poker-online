e4e9cf3cde7605038a2261ef6217ce22887371b8
GusNitrous <gusnitrous@gmail.com>
2022-10-03 01:40:36 +0300

    Added: autohiding snackbar message

--- src/components/AppAlert/AppAlert.jsx
+++ src/components/AppAlert/AppAlert.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Slide, Snackbar} from "@material-ui/core";
 import {Alert} from "@material-ui/lab";
 import {useStore} from "effector-react";
 import {$wsState, resetWsErrors} from "../../models/ws";
 import {$appState, resetAppErrors} from "../../models/app";
 
+
+const AUTO_HIDE_DURATION = 3000;
 
 export const AppAlert = () => {
     const {error, exception} = useStore($wsState);
@@ -21,15 +23,27 @@ export const AppAlert = () => {
         }
     }
 
+    useEffect(() => {
+        let timer;
+        if (hasErrors) {
+            timer = setTimeout(() => {
+                handleClose();
+                clearTimeout(timer);
+            }, AUTO_HIDE_DURATION);
+        }
+        return () => clearTimeout(timer);
+    }, [hasErrors]);
+
     return <Snackbar
-        TransitionComponent={(props) => <Slide {...props} direction="up"/>}
-        anchorOrigin={{vertical: "bottom", horizontal: "center"}}
-        open={hasErrors}
+            autoHideDuration={AUTO_HIDE_DURATION}
+            TransitionComponent={(props) => <Slide {...props} direction="up"/>}
+            anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+            open={hasErrors}
     >
         <Alert
-            style={{width: '100%'}}
-            onClose={handleClose}
-            severity={networkError?.level || "error"}>
+                style={{width: '100%'}}
+                onClose={handleClose}
+                severity={networkError?.level || "error"}>
             {errorMessage}
         </Alert>
     </Snackbar>
